69b6a384ef7898ea7e88f23c7f4f05d75fbaf17d
ttn.dev.fullstack <nghiatt@qasoftsolution.com>
2024-03-11 20:59:44 +0700

    Init application

---
+++ HRMManagement/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace HRMManagement.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
