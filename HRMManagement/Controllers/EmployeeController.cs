288f7615c51262ebbe3e8ec7ed060215c07701af
Tan Kiet Nguyen <kiettuanleo@gmail.com>
2024-03-18 13:30:30 +0700

    Quan ly nhan vien + Sidebar layout + Emp Controler

---
+++ HRMManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HRMManagement.Models; 
+
+public class EmployeeController : Controller
+{
+    private readonly EmpDBContext _context;
+
+    public EmployeeController(EmpDBContext context)
+    {
+        this._context = context;
+    }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        //
+        var employees = _context.NHANVIENs.ToList();
+        List<EmployeesViewModel> employeeList = new List<EmployeesViewModel>();
+
+        //var employeesWithRelatedData = _context.NHANVIENs
+        //    .Include(e => e.BANGCAPs)
+        //    .Include(e => e.BAOHIEMs)
+        //    .Include(e => e.CHAMCONGs)
+        //    .Include(e => e.CHUCVU)
+        //    .Include(e => e.DAOTAONHANVIENs)
+        //    .Include(e => e.HOPDONGs)
+        //    .Include(e => e.LUONGs)
+        //    .Include(e => e.PHONGBAN)
+        //    .Include(e => e.TAIKHOANs)
+        //    .Include(e => e.TKNGANHANGs)
+        //    .Include(e => e.YEUCAUNGHIPHEPs)
+        //    .ToList();
+
+        if (employees != null)
+        { 
+            foreach( var employee in employees) 
+            { 
+                var EmployeesViewModel = new EmployeesViewModel() 
+                {
+                    ID = employee.ID,
+                    HoDem = employee.HoDem,
+                    Ten = employee.Ten,
+                    NgaySinh = employee.NgaySinh,
+                    GioiTinh = employee.GioiTinh,
+                    CCCD = employee.CCCD,
+                    DiaChi = employee.DiaChi,
+                    SDT = employee.SDT,
+                    Email = employee.Email,
+                    QueQuan = employee.QueQuan,
+                    NgayTuyenDung = employee.NgayTuyenDung,
+                    AnhProfile = employee.AnhProfile,
+                    IDViTri = employee.IDViTri,
+                    IDChucVu = employee.IDChucVu,
+                    IDPhongBan = employee.IDPhongBan,
+                };
+                employeeList.Add(EmployeesViewModel);
+            }
+            return View(employeeList);
+        }
+
+        return View(employeeList);
+    }
+
+    // Display a single Emp
+    //public IActionResult Display(String id)
+    //{
+    //    var Emp = _productRepository.GetById(id);
+    //    if (Emp == null)
+    //    {
+    //        return NotFound();
+    //    }
+    //    return View(Emp);
+    //}
+}
---
+++ HRMManagement/Views/Employee/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HRMManagement.Data;
+using HRMManagement.Models;
+
+namespace HRMManagement.Views.Employee
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly HRMManagement.Data.HRMManagementContext _context;
+
+        public DeleteModel(HRMManagement.Data.HRMManagementContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public EmployeesViewModel EmployeesViewModel { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.EmployeesViewModel == null)
+            {
+                return NotFound();
+            }
+
+            var employeesviewmodel = await _context.EmployeesViewModel.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (employeesviewmodel == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                EmployeesViewModel = employeesviewmodel;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null || _context.EmployeesViewModel == null)
+            {
+                return NotFound();
+            }
+            var employeesviewmodel = await _context.EmployeesViewModel.FindAsync(id);
+
+            if (employeesviewmodel != null)
+            {
+                EmployeesViewModel = employeesviewmodel;
+                _context.EmployeesViewModel.Remove(EmployeesViewModel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
