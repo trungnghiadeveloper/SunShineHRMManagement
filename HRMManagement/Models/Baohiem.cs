451a76f777851caa3fd1bde08e4899c76b9cec03
agustdj <lehieukien2206@gmail.com>
2024-03-20 00:01:32 +0700

    DB

---
+++ HRMManagement/Models/Baohiem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRMManagement.Models;
+
+public partial class Baohiem
+{
+    public int IdsoBaoHiem { get; set; }
+
+    public DateTime? NgayHienHanh { get; set; }
+
+    public string? TrangThai { get; set; }
+
+    public int? Idloai { get; set; }
+
+    public string? Idnv { get; set; }
+
+    public virtual Loaibh? IdloaiNavigation { get; set; }
+
+    public virtual Nhanvien? IdnvNavigation { get; set; }
+}
