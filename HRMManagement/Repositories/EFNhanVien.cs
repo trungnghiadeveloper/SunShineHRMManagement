d55ca0ebfbfb91b679a1ddc75df364a6aa9ae4e1
agustdj <lehieukien2206@gmail.com>
2024-03-23 14:33:33 +0700

    Detail Info

---
+++ HRMManagement/Repositories/EFNhanVien.cs
@@ -0,0 +1,35 @@
+﻿using HRMManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace HRMManagement.Repositories
+{
+    public class EFNhanVien : INhanVien
+    {
+        private readonly HrmContext _context;
+
+        public EFNhanVien(HrmContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Nhanvien>> GetAllAsync()
+        {
+            return await _context.Nhanviens.ToListAsync();
+        }
+
+        public async Task<Nhanvien> GetByIdAsync(string id)
+        {
+            return   await _context.Nhanviens.FindAsync(id);
+            
+        }
+
+        public Task<IActionResult> Updateimage(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
