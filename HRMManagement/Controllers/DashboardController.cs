d4f16e06ae658abeb1d95a34ab72a9e65c82486f
trungnghiadeveloper <nghiatruong14102003@gmail.com>
2024-04-13 18:45:05 +0700

    feat: dashboard page

--- HRMManagement/Controllers/DashboardController.cs
+++ HRMManagement/Controllers/DashboardController.cs
@@ -1,20 +1,33 @@
-﻿using HRMManagement.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using HRMManagement.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HRMManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Text;
+using System.Resources;
 using System.Diagnostics;
 
 namespace HRMManagement.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly ILogger<DashboardController> _logger;
+        private readonly INhanVien _nhanvien;
+        private readonly IChucVu _chucvu;
+        private readonly HrmContext _context;
 
-        public DashboardController(ILogger<DashboardController> logger)
+        public DashboardController(HrmContext context, INhanVien nhanvien)
         {
-            _logger = logger;
+            _context = context;
+            _nhanvien = nhanvien;
         }
 
         public IActionResult Index()
         {
+            Dictionary<string, int> stats = getStats();
+
+            ViewData["stats"] = stats;
+
             return View();
         }
 
@@ -22,6 +35,17 @@ namespace HRMManagement.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public Dictionary<string, int> getStats()
+        {
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+
+            int nhanvien = _context.Nhanviens.Count();
+
+            stats.Add("nhanvien", nhanvien);
+
+            return stats;
         }
     }
 }
