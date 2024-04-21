c09ddff3cbc25f0c372d2c18657d6035c596fa1c
trungnghiadeveloper <nghiatruong14102003@gmail.com>
2024-04-21 14:13:42 +0700

    update

--- HRMManagement/Models/PaginatedList.cs
+++ HRMManagement/Models/PaginatedList.cs
@@ -1,32 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRMManagement.Models
 {
-    public class PaginatedList<T>
+    public class PaginatedList<T> : List<T>
     {
-        public List<T>? Items { get; set; }
-        public int TotalItems { get; set; }
         public int PageIndex { get; set; }
-        public int Pagesize { get; set; }
         public int TotalPages { get; private set; }
 
         //
-       public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-            Items = items;
+            this.AddRange(items);
         }
 
         public bool HasPreviousPage => (PageIndex > 1);
-        public bool HasNext => (PageIndex < TotalPages);
-        public int FirstItemIndex => (PageIndex - 1) * Pagesize + 1;
-        public int LastItemIndex => Math.Min(PageIndex * Pagesize, TotalItems);
+        public bool HasNextPage => (PageIndex < TotalPages);
+        //public int FirstItemIndex => (PageIndex - 1) * Pagesize + 1;
+        //public int LastItemIndex => Math.Min(PageIndex * Pagesize, TotalItems);
 
-        internal static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static PaginatedList<T> Create(List<T> source, int pageIndex, int pageSize)
         {
-            var count = await source.CountAsync();  //
-            var items = await source.Skip((pageIndex - 1)* pageSize).Take(pageSize).ToListAsync();
+            var count = source.Count();  //
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
